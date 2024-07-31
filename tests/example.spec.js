fbb1d984285f3c64e4344f5c684b66dfb568bca7
Khadeerunnisa Shaik <110010732+khadeerun@users.noreply.github.com>
2024-07-31 10:02:03 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,68 +1,64 @@
 import { test } from '@playwright/test';
 const { Details } = require('../pageObject/playwrighttest.ts');
 
-
-
-test.describe('To Group', () => {
+test.describe.serial('ContactApptestStarted', () => {
 test('SignUp Page', async ({ page }) => {
 const obj = new Details(page);
 await obj.openBrowser();
 await obj.SignUp();
 await obj.page.waitForTimeout(5000);
-await obj.ContactList(); 
-await obj.page.waitForTimeout(5000);
-//await obj.AddContact();
 await obj.page.pause();
 });
 
 test('Add Contact Page', async ({ page }) => {
   const obj = new Details(page);
+  
   await obj.openBrowser();
   await obj.LogIn();
   await obj.AddContact();
   await obj.page.waitForTimeout(5000);
-  await obj.LogOut();
+  await obj.ContactDetails();
   await obj.page.pause();
   });
 
-  test('ContactListDetails', async ({ page }) => {
+ /* test('ContactListDetails', async ({ page }) => {
     const obj = new Details(page);
    await obj.openBrowser();
-   await obj.page.waitForTimeout(5000);
    await obj.LogIn();
    await obj.page.waitForTimeout(5000);
    await obj.ContactListDetails();
    await obj.page.waitForTimeout(5000);
    await obj.page.pause();
-  });
+  });*/
 
 test('EditContact', async ({ page }) => {
   const obj = new Details(page);
+ 
+
  await obj.openBrowser();
- await obj.page.waitForTimeout(5000);
  await obj.LogIn();
- await obj.page.waitForTimeout(5000);
- await obj.ContactListDetails();
+ await obj.ContactDetails();
  await obj.page.waitForTimeout(5000);
  await obj.EditContact();
- //await obj.page.waitForTimeout(5000);
- //await obj.UpdatedContactDetails();
- //await obj.page.waitForTimeout(5000);
+ 
  await obj.page.pause();
 });
 
-/*test('Delete', async ({ page }) => {
+ 
+
+
+test.only('Delete', async ({ page }) => {
   const obj = new Details(page);
  await obj.openBrowser();
- await obj.page.waitForTimeout(5000);
+ 
  await obj.LogIn();
  await obj.page.waitForTimeout(5000);
  await obj.ContactDetails();
  await obj.page.waitForTimeout(5000);
  await obj.Delete();
- await obj.page.waitForTimeout(5000);
+
  await obj.page.pause();
-});*/
+});
 
 });
 
